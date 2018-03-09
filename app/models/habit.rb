e5dc7737d99b3b58b31779e87e13032a42894bb9
Raghu Betina <raghu@firstdraft.com>
2018-03-09 00:32:36 +0000

    Validate uniqueness of habits.name

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -5,6 +5,8 @@ class Habit < ApplicationRecord
 
   # Validations
 
+  validates :name, :uniqueness => { :message => "You are already working on this habit!" }
+
   validates :name, :presence => { :message => "What specific habit would you like to develop?" }
 
   validates :name, :length => { :minimum => 1, :maximum => 50 }
