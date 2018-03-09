990bb1f09f04bcaf545ac95a452b19dae4b459a6
Raghu Betina <raghu@firstdraft.com>
2018-03-09 00:32:36 +0000

    Validate length of habits.goal

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -5,6 +5,8 @@ class Habit < ApplicationRecord
 
   # Validations
 
+  validates :goal, :length => { :minimum => 1, :maximum => 50 }
+
   validates :name, :uniqueness => { :message => "You are already working on this habit!" }
 
   validates :name, :presence => { :message => "What specific habit would you like to develop?" }
