8cb63ea3709556fa525e8a6fc8a2dcfcb627ee6a
Raghu Betina <raghu@firstdraft.com>
2018-03-09 00:32:36 +0000

    Validate uniqueness of aspirations.name

--- app/models/aspiration.rb
+++ app/models/aspiration.rb
@@ -5,6 +5,8 @@ class Aspiration < ApplicationRecord
 
   # Validations
 
+  validates :name, :uniqueness => { :message => "The aspiration already exists!" }
+
   validates :name, :presence => { :message => "What is your aspiration?" }
 
   validates :name, :length => { :minimum => 1, :maximum => 30 }
