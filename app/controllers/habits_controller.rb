850736d2050c2a636e80906df19629113ba6a357
Raghu Betina <raghu@firstdraft.com>
2018-03-09 00:33:18 +0000

    Only habit user can modify habit

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,14 @@
 class HabitsController < ApplicationController
+  before_action :current_user_must_be_habit_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_habit_user
+    habit = Habit.find(params[:id])
+
+    unless current_user == habit.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @habits = Habit.all
 
