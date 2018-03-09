9fc76ab4e26d321b13a11b0585a4092e522219ec
Raghu Betina <raghu@firstdraft.com>
2018-03-09 00:32:52 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "aspirations#index"
   # Routes for the Accomplishment resource:
   # CREATE
